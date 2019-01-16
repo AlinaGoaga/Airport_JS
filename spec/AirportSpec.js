3b73b1f1a73c36ac0cbf7ba7ac53b3f3031f1c6a
Alina Goaga <goaga.alina@gmail.com>
2019-01-16 22:14:50 +0000

    Refactored to use a clone for plane in the airport spec

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -4,21 +4,18 @@ describe("Airport", function() {
 
   beforeEach(function() {
     airport = new Airport();
-    plane1 = new Plane('BA123');
-    plane2 = new Plane('KLM567')
+    plane = 'plane';
   });
 
   it("is able to land planes", function() {
-    airport.land(plane1);
-    airport.land(plane2);
-    expect(airport.planesFlightCodes()).toEqual(['BA123','KLM567']);
+    airport.land(plane);
+    expect(airport.planesQueue).toContain(plane);
   });
 
   it("is able to take off a plane", function() {
-    airport.land(plane1);
-    airport.land(plane2);
-    airport.takeoff(plane1);
-    expect(airport.planesFlightCodes()).toEqual(['KLM567']);
+    airport.land(plane);
+    airport.takeoff(plane);
+    expect(airport.planesQueue).toEqual([]);
   });
 
 });
